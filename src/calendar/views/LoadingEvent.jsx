c7f9a592db40288acae79e3dd2603edc09f4ae37
Kevin04C <kevincespedes2004@gmai.com>
2022-11-13 12:08:05 -0500

    fix class to ClassName

--- src/calendar/views/LoadingEvent.jsx
+++ src/calendar/views/LoadingEvent.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export const LoadingEvent = () => {
   return (
-    <div class="d-flex justify-content-center mt-5">
-      <div class="spinner-border" role="status" style={{width: '3rem', height: '3rem'}}>
-        <span class="visually-hidden">Loading...</span>
+    <div className="d-flex justify-content-center mt-5">
+      <div className="spinner-border" role="status" style={{width: '3rem', height: '3rem'}}>
+        <span className="visually-hidden">Loading...</span>
       </div>
     </div>
   );
